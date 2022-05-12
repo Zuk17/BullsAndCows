55030424742e7f863123738f3a84c47f667dbe39
Ivan Zhukov <ivan.p.zhukov@gmail.com>
2022-05-12 20:40:13 +0300

    stage 6 of 7

--- Bulls and Cows/task/src/bullscows/InputStr.java
+++ Bulls and Cows/task/src/bullscows/InputStr.java
@@ -4,25 +4,45 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputStr {
-    private Scanner scanner;
+    private final Scanner scanner;
 
     public InputStr() {
         scanner = new Scanner(System.in);
     }
 
     public int readCount() {
-        int count = 0;
+        int count;
         do {
-            System.out.println("Please, enter the secret code's length:");
+            System.out.println("Input the length of the secret code:");
             try {
-                count = Integer.valueOf(scanner.nextLine());
+                count = Integer.parseInt(scanner.nextLine());
             } catch (InputMismatchException e) {
                 continue;
             }
 
-            if (count > 10) {
+            if (count > 36) {
                 System.out.println("Error: can't generate a secret number with " +
                         "a length of " + count + " because there aren't enough unique digits.");
+                continue;
+            }
+            break;
+        } while (true);
+        return count;
+    }
+
+    public int readPossible() {
+        int count = -1;
+        do {
+            System.out.println("Input the number of possible symbols in the code:");
+            try {
+                count = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+
+            if (count > 36) {
+                System.out.println("Error: can't generate a secret number with " +
+                        count + " numbers variable. because we can only 0-9, a-z.");
                 continue;
             }
             break;
@@ -35,11 +55,11 @@ public class InputStr {
         if (scanner.hasNext()) {
             try {
                 answer = scanner.nextLine();
-                if (!answer.matches("[0-9]*") || answer.length() != codeLength) {
+                if (!answer.matches("[0-9, a-z]*") || answer.length() != codeLength) {
                     System.out.println("Wrong input answer.");
                     System.out.println("Enter correct length.\n code.length = " + codeLength +
                             "\t\tanswer.length = " + answer.length() + "\n" +
-                            "answer = \'" + answer + "\'");
+                            "answer = '" + answer + "'");
                     answer = "";
                 }
             } catch (InputMismatchException e) {
@@ -49,6 +69,7 @@ public class InputStr {
             System.out.println("Exiting by end of scanner");
             return null;
         }
+        assert answer != null;
         return answer.toCharArray();
     }
 }
--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,89 +1,30 @@
 package bullscows;
 
-import java.util.*;
+import static bullscows.Checker.exit;
 
 public class Main {
-
-    static char[] code;
-    static boolean exit = false;
 
     public static void main(String[] args) {
         InputStr inputStr = new InputStr();
 
-        code = generateSecret(inputStr.readCount());
+        int lengthCode = inputStr.readCount();
+        int possibleElements = inputStr.readPossible();
+        char[] secretCode = SecretCode.generate(lengthCode, possibleElements);
 
+        assert secretCode != null;
+        System.out.println("secret code = " + String.valueOf(secretCode));
         System.out.println("Okay, let's start a game!");
-        guessCode(inputStr);
-    }
 
-    private static char[] generateSecret(int count) {
-        char[] code = new char[count];
-        char[] pseudoRandom = String.valueOf(System.nanoTime()).toCharArray();
-        int j = 0;
-        HashSet tmpHash = new HashSet<>();
-        for (int i = pseudoRandom.length - 1; i >= 0; i--) {
-            if (j == 0 && pseudoRandom[i] != '0') {
-                code[j] = pseudoRandom[i];
-                j++;
-                tmpHash.add(pseudoRandom[i]);
-            }
-            if (j != 0 && tmpHash.add(pseudoRandom[i])) {
-                code[j] = pseudoRandom[i];
-                j++;
-            }
-            if (j > count - 1) break;
-        }
-        return code;
-    }
-
-    private static void guessCode(InputStr inputStr) {
         int counter = 0;
         do {
             counter++;
             System.out.println("Turn " + counter + ". Answer:");
-            String str = checkBulls(inputStr.nextAnswer(code.length));
+            String str = Checker.checkBulls(inputStr.nextAnswer(lengthCode), secretCode);
             if (str == null) break;
 
             System.out.println("Grade: " + str + ".\n");
 
         } while (!exit);
         if (exit) System.out.println("Congratulations! You guessed the secret code.");
-    }
-
-    private static String checkBulls(char[] answer) {
-        int bulls = 0;
-        int cows = 0;
-        HashSet<Character> tmp = new HashSet<>();
-        for (char a : code) tmp.add(a);
-
-        assert (answer != null);
-        if (code.length != answer.length) {
-            System.out.println("Enter correct length.\n code.length = " + code.length +
-                    "\t\tanswer.length = " + answer.length + "\n" +
-                    "code = " + String.valueOf(code) + "\t\t answer = \'" +
-                    String.valueOf(answer) + "\'");
-            return "None";
-        }
-
-        for (int i = 0; i < code.length; i++) {
-            if (code[i] == answer[i]) bulls++;
-            else if (!tmp.add(answer[i])) cows++;
-        }
-
-        String strBulls = null, strCows = null;
-        if (bulls != 0) {
-            strBulls = bulls + " bull" + (bulls == 1 ? "" : "s");
-        }
-        if (cows != 0) {
-            strCows = cows + " cow" + (cows == 1 ? "" : "s");
-        }
-
-        if (bulls == code.length) exit = true;
-        String str;
-        if (bulls + cows == 0) str = "None";
-        else if (bulls * cows != 0) str = strBulls + " and " + strCows;
-        else if (bulls != 0) str = strBulls;
-        else str = strCows;
-        return str;
     }
 }
